8b172df831212ba2df25601b9e1a19ea58d092cf
Tamara Vahinerii Flosse <tflosse@tamaras-mbp.home>
2020-06-10 18:04:25 -0700

    finished nav and about section on mobile

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
 console.log('hello')
+
+// $(() => {
+
+//   const $navButtons = $('nav button');
+//   const $nav = $('nav');
+//   $navButtons.on('click', () => {
+//       $nav.toggleClass('open');
+//   })
+//   $(window).on('resize', () => {
+//       console.log(window.innerWidth);
+//       if (window.innerWidth >768) {
+//           $nav.remove('open');
+//       }
+//   })
+
+// })
+
+$('.header-text').fadeTo(2500, 1);
+
+const $navButtons = $('nav button');
+const $nav = $('nav');
+$navButtons.on('click', () => {
+    $nav.toggleClass('open');
+})
+
+// $('nav a').on('click', () => {
+//   $nav.remove('open');
+// })
